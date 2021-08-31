e9e246f57eec9d89f6b12db4f43a73e98c3cc512
William Derek Dias <williamdkdevops@gmail.com>
2021-08-31 06:31:54 -0300

    Implemented classes model

--- shopping-cart/src/main/java/br/com/supernova/shoppingcart/model/Cart.java
+++ shopping-cart/src/main/java/br/com/supernova/shoppingcart/model/Cart.java
@@ -1,9 +1,20 @@
 package br.com.supernova.shoppingcart.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@RedisHash("cart")
 public class Cart {
 
+    @Id
     private Integer id;
     private List<Item> items;
 
--- shopping-cart/src/main/java/br/com/supernova/shoppingcart/model/Item.java
+++ shopping-cart/src/main/java/br/com/supernova/shoppingcart/model/Item.java
@@ -1,7 +1,19 @@
 package br.com.supernova.shoppingcart.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@RedisHash
 public class Item {
 
+    @Id
     private Integer productId;
     private Integer amount;
+
 }
